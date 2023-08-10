ec2eeacf03c1d9dc51afc4c1c1464ced875e4fca
kristian iverin <kristian.alexander.iverin@tietoevry.com>
2023-08-10 13:45:21 +0200

    wip

--- Server/Controllers/ProductController.cs
+++ Server/Controllers/ProductController.cs
@@ -1,9 +1,6 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using BlazorEcommerce.Shared;
+
 
 namespace BlazorEcommerce.Server.Controllers
 {
@@ -11,36 +8,42 @@ namespace BlazorEcommerce.Server.Controllers
     [ApiController]
     public class ProductController : ControllerBase
     {
-        // GET: api/Product
+        //List of dummy items
+        private static List<Product> Products = new List<Product>
+        {
+            new Product
+            {
+            Id = 1,
+            Title = "The Hitchhiker's Guide to the Galaxy",
+            Description = "The Hitchhiker's Guide to the Galaxy is an international multimedia phenomenon; the novels are the most widely distributed, having been translated into more than 30 languages by 2005.[4][5] The first novel, The Hitchhiker's Guide to the Galaxy (1979), has been ranked fourth on the BBC's The Big Read poll.",
+            ImageUrl = "https://upload.wikimedia.org/wikipedia/en/b/bd/H2G2_UK_front_cover.jpg",
+            Price = 9.99m
+            },
+
+            new Product
+            {
+            Id = 2,
+            Title = "Ready Player One",
+            Description = "Ready Player One is a 2011 science fiction novel, and the debut novel of American author Ernest Cline. The story, set in a dystopia in 2045, follows protagonist Wade Watts on his search for an Easter egg in a worldwide virtual reality game, the discovery of which would lead him to inherit the game creator's fortune. Cline sold the rights to publish the novel in June 2010",
+            ImageUrl = "https://upload.wikimedia.org/wikipedia/en/thumb/a/a4/Ready_Player_One_cover.jpg/220px-Ready_Player_One_cover.jpg",
+            Price = 9.99m
+            },
+
+            new Product
+            {
+            Id = 3,
+            Title = "Nineteen Eighty-Four",
+            Description = "Nineteen Eighty-Four (also published as 1984) is a dystopian social science fiction novel and cautionary tale by English writer George Orwell. It was published on 8 June 1949 by Secker & Warburg as Orwell's ninth and final book completed in his lifetime. Thematically, it centres on the consequences of totalitarianism, mass surveillance and repressive regimentation of people and behaviours within society.",
+            ImageUrl = "https://upload.wikimedia.org/wikipedia/commons/thumb/c/c3/1984first.jpg/220px-1984first.jpg",
+            Price = 9.99m
+            }
+            };
+
         [HttpGet]
-        public IEnumerable<string> Get()
+        public async Task<IActionResult> GetProduct()
         {
-            return new string[] { "value1", "value2" };
+            return Ok(Products);
         }
 
-        // GET: api/Product/5
-        [HttpGet("{id}", Name = "Get")]
-        public string Get(int id)
-        {
-            return "value";
-        }
-
-        // POST: api/Product
-        [HttpPost]
-        public void Post([FromBody] string value)
-        {
-        }
-
-        // PUT: api/Product/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
-        {
-        }
-
-        // DELETE: api/Product/5
-        [HttpDelete("{id}")]
-        public void Delete(int id)
-        {
-        }
     }
 }
