712ae351c64b66a2fe51ac47291eee04d963ce21
kristian iverin <kristian.alexander.iverin@tietoevry.com>
2023-08-14 21:21:33 +0200

    working on server side controller

--- Server/Controllers/ProductController.cs
+++ Server/Controllers/ProductController.cs
@@ -2,6 +2,15 @@ using Microsoft.AspNetCore.Mvc;
 using BlazorEcommerce.Shared;
 using BlazorEcommerce.Server.Data;
 
+
+
+/* Here is the explanation for the code.
+1. We defined the route and the controller name. 
+2. We defined the DataContext class as _context.
+3. We defined the constructor of ProductController and we passed the DataContext.
+4. We created a method GetProducts to get the list of products.
+We used the await keyword to wait for the request to finish and we set the result to Products.
+Then, we returned the result as an OkObjectResult. OkObjectResult is a status code 200. */
 namespace BlazorEcommerce.Server.Controllers
 {
     [Route("api/[controller]")]
@@ -16,11 +25,16 @@ namespace BlazorEcommerce.Server.Controllers
         }
 
         [HttpGet]
-        public async Task<ActionResult<List<Product>>> GetProducts()
+        public async Task<ActionResult<ServiceResponse<List<Product>>>> GetProducts()
         {
-            var Products = await _context.Products.ToListAsync();
-            return Ok(Products);
+            var products = await _context.Products.ToListAsync();
+            var response = new ServiceResponse<List<Product>>()
+            {
+                Data = products
+            };
+            return Ok(response);
         }
+
 
     }
 }
